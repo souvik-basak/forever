97c4404dadb82428f4bdf81143493eee567a96bf
Souvik <souvik.basak2404@gmail.com>
2024-10-16 03:02:40 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port:5173}
-})
+  server: { port: 5173 },
+  build: {
+    rollupOptions: {
+      external: ["react-router-dom"],
+    },
+  },
+});
+
