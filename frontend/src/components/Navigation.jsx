143d16db6f8ddc6399a46febc989f8ab501ae2f8
Souvik <souvik.basak2404@gmail.com>
2024-10-16 02:44:06 +0530

    updated commit

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { assets } from "../assets/frontend_assets/assets.js";
 import { Link, NavLink } from "react-router-dom";
-import { ShopContext } from "../context/shopContext.jsx";
+import { ShopContext } from "../context/ShopContext.jsx";
 const Navigation = () => {
   const [visible, setVisible] = useState(false);
   const toggleMenu = (e) => {
