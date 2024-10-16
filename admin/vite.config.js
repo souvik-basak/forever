55ba852f26e28bc07414558282fa5d0846271fd6
Souvik <souvik.basak2404@gmail.com>
2024-10-16 12:26:16 +0530

    new

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import styleImport from "vite-plugin-style-import";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    styleImport({
-      libs: [
-        {
-          libraryName: "react-toastify",
-          esModule: true,
-          resolveStyle: () => "react-toastify/dist/ReactToastify.css",
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
   server: { port: 5174 },
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom", "react-toastify"],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ["react-router-dom", "react-toastify"],
+  //   },
+  // },
 });
 
