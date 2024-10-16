c62471c6dd7b7667600224c0218770f36d3b4df0
Souvik <souvik.basak2404@gmail.com>
2024-10-16 19:43:28 +0530

    new

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext";
-import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
