e6cb9c1f1e5bd1298bff22409685f19a649f96ac
Souvik <souvik.basak2404@gmail.com>
2024-10-16 14:21:57 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,14 +11,14 @@ import Orders from './pages/Orders'
 import Navigation from './components/Navigation'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Verify from './pages/Verify'
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      {/* <ToastContainer /> */}
+      <ToastContainer />
       <Navigation/>
       <SearchBar/>
       <Routes>
